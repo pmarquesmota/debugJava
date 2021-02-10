0c7160518738eb075acf0ae35de85585cb3e778e
Paul Marques Mota <paul@marquesmota.pt>
2021-02-10 18:22:01 +0100

    move main loop to its own interface and class

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,48 +1,15 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class AnalyticsCounter {
-	private static HashSet<String> symptomData;
 	private static HashMap<String, Integer> result = new HashMap<String, Integer>();
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		ISymptomReader symptomReader = new ReadSymptomDataFromFile();
-		symptomReader.InitializeFile("symptomData.txt");
 		
-		symptomData = symptomReader.GetSymptoms();
-		
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			
-			for(String symptom : symptomData) {
-				if(line.contains(symptom)) {
-		            // Check if the element is present 
-		            Integer count = result.get(symptom); 
-		            
-		            // If this is first occurrence of element 
-		            // Insert the element 
-		            if (count == null) { 
-		            	result.put(symptom, 1); 
-		  
-		            // If elements already exists in hash map 
-		            // Increment the count of element by 1 
-					} else { 
-		            	result.put(symptom, ++count); 
-		            } 
-				}
-			}
-			
-			line = reader.readLine();	// get another symptom
-		}
-		reader.close();
+		IMainLoop loop = new DoMainLoop();
+		loop.InitializeFile("symptoms.txt");
+		result = loop.MainLoop();
 		
 		IWriteFile writeFile = new WriteToFile();
 		writeFile.InitializeFile("result.out");
