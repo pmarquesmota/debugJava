0a949712c77cb822f2b5a9f45dfbaf2089521f23
Paul Marques Mota <paul@marquesmota.pt>
2021-02-10 18:24:07 +0100

    move global var to local

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,9 +3,9 @@ package com.hemebiotech.analytics;
 import java.util.HashMap;
 
 public class AnalyticsCounter {
-	private static HashMap<String, Integer> result = new HashMap<String, Integer>();
 	
 	public static void main(String args[]) throws Exception {
+		HashMap<String, Integer> result = new HashMap<String, Integer>();
 		
 		IMainLoop loop = new DoMainLoop();
 		loop.InitializeFile("symptoms.txt");
