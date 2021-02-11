41476ccd82e11722c489cb9fbb8d6fec6d02f2c6
Paul Marques Mota <paul@marquesmota.pt>
2021-02-11 14:52:50 +0100

    move to proper packages

--- src/com/hemebiotech/analytics/IMainLoop.java
+++
@@ -1,8 +0,0 @@
-package com.hemebiotech.analytics;
-
-import java.util.TreeMap;
-
-public interface IMainLoop {
-	void InitializeFile (String filepath);
-	TreeMap<String, Integer> MainLoop();
-}
--- src/com/hemebiotech/analytics/IWriteFile.java
+++
@@ -1,8 +0,0 @@
-package com.hemebiotech.analytics;
-
-import java.util.TreeMap;
-
-public interface IWriteFile {
-	void InitializeFile (String filepath);
-	void WriteFile(TreeMap<String, Integer> result);
-}
