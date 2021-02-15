4f60d5ebbe60aac84c5f1e4cd080dd1ca02fd575
Paul Marques Mota <paul@marquesmota.pt>
2021-02-15 14:18:34 +0100

    add comment to the main loop interface

--- src/com/hemebiotech/analytics/mainLoop/IMainLoop.java
+++ src/com/hemebiotech/analytics/mainLoop/IMainLoop.java
@@ -2,6 +2,11 @@ package com.hemebiotech.analytics.mainLoop;
 
 import java.util.TreeMap;
 
+/**
+ * 
+ * Main loop class. Initialize the input file and implements the main loop
+ *
+ */
 public interface IMainLoop {
 	void InitializeFile (String inputFile);
 	TreeMap<String, Integer> MainLoop();
