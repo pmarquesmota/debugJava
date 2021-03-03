468b6fe16cdd3a56f1044870ccb34c4ab84da534
Paul Marques Mota <paul@marquesmota.pt>
2021-03-03 18:07:58 +0100

    use main class in main()

--- src/com/hemebiotech/analytics/main/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/main/AnalyticsCounter.java
@@ -1,28 +1,15 @@
 package com.hemebiotech.analytics.main;
 
-import java.util.TreeMap;
-
-import com.hemebiotech.analytics.mainLoop.DoMainLoop;
-import com.hemebiotech.analytics.mainLoop.IMainLoop;
-import com.hemebiotech.analytics.write.IWriteFile;
-import com.hemebiotech.analytics.write.WriteToFile;
 
 public class AnalyticsCounter {
 	
 	/**
 	 * 
-	 * Main method. Calls the main loop method then the method that writes the result to the output file
+	 * Main method. Calls the main class
 	 */
 	public static void main(String args[]) throws Exception {
-		TreeMap<String, Integer> result = new TreeMap<String, Integer>();
-		
-		IMainLoop loop = new DoMainLoop();
-		loop.initializeFile("symptoms.txt");
-		result = loop.extractSymptoms();
-		
-		IWriteFile writeFile = new WriteToFile();
-		writeFile.initializeFile("result.out");
-		writeFile.writeFile(result);
+		MainClass mainClass = new MainClass();
+		mainClass.doMainClass();
 	}
 	
 }
